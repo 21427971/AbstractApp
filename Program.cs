1f83dbaa80c73dd3fe7e20331e9b07113a3de808
Your Name <you@example.com>
2023-04-23 22:25:35 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractApp
+{
+    abstract class MyShape
+
+    {
+        public abstract int Area();
+
+     }
+    class Rectangle:MyShape
+    {
+        private int Length;
+        private int Width;
+
+        public Rectangle(int ar=0,int ba=0)
+        {
+            Length = ar;
+            Width = ba;
+        }
+        public override int Area()
+        {
+            Console.WriteLine("Rectangle class area :");
+            return (Length * Width);
+        }
+        class RectangleTester
+        {
+            static void Main(string[] args)
+            {
+                Rectangle r = new Rectangle(10, 7);
+                double a = r.Area();
+                Console.WriteLine($"Area is {a}");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
